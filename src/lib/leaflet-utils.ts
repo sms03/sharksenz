0a899c5c00f57294b9291442ba6d9aa5c693c6ee
Shivam M. Salunkhe <84768081+sms03@users.noreply.github.com>
2025-05-23 15:49:30 +0530

    Add Leaflet CSS and utility functions for map icon handling

---
+++ src/lib/leaflet-utils.ts
@@ -0,0 +1,24 @@
+// Helper file to fix Leaflet marker icon issues
+import L from 'leaflet';
+
+// Fix Leaflet marker icon issue
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+export const MapIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  shadowSize: [41, 41],
+});
+
+// Helper function to check if code is running in browser
+export const isBrowser = () => typeof window !== 'undefined';
